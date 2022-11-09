37704cf4b8710ce93c33e954dfa176a4c7519e67
cuam3a <gcuameatelles@gmail.com>
2022-11-08 18:38:08 -0700

    update img

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -6,11 +6,11 @@ export default function Navbar() {
     const router = useRouter()
 
     return (
-        <div className='w-full grid grid-cols-1 md:grid-cols-8'>
+        <div className='w-full grid grid-cols-1 md:grid-cols-8 bg-white shadow'>
             <div className='col-span-1 text-center flex justify-center items-center text-gray-800 font-mono font-semibold'>
                 GCUAMEA
             </div>
-            <nav className="bg-white shadow dark:bg-gray-800 col-span-6">
+            <nav className=" dark:bg-gray-800 col-span-6">
                 <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
                     <Link href="/" className={`text-gray-800 font-mono font-semibold dark:text-gray-200 border-b-2 ${router.pathname == "/" ? "border-blue-500" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200"} mx-1.5 sm:mx-6`}>INICIO</Link>
                     <Link href="/conocimientos" className={`text-gray-800 font-mono font-semibold dark:text-gray-200 border-b-2 ${router.pathname == "/conocimientos" ? "border-blue-500" : "border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200"} mx-1.5 sm:mx-6`}>CONOCIMIENTOS</Link>
@@ -19,7 +19,7 @@ export default function Navbar() {
                 </div>
             </nav>
             <div className='col-span-1 text-center flex justify-center items-center text-gray-800 font-mono font-semibold'>
-                <Link href="www.linkedin.com/in/guillermo-cuamea-telles-284308138" target="_blank" className='ml-2'><BsLinkedin /></Link>
+                <Link href="https://www.linkedin.com/in/guillermo-cuamea-telles-284308138" target="_blank" className='ml-2'><BsLinkedin /></Link>
                 <Link href="https://github.com/cuam3a" target="_blank" className='ml-2'><BsGithub /></Link>
             </div>
         </div>
--- pages/portafolio.js
+++ pages/portafolio.js
@@ -57,7 +57,8 @@ export default function Portfolio() {
                         </p>
                     </div>
                     <div>
-                        <Link href="https://github.com/cuam3a/store-app" target="_blank" className='flex justify-center' ><span className="inline-block bg-blue-500 text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 w-3/5 flex flex-row items-center justify-center"><BsGithub className='mr-2'/> Repositorio</span></Link>
+                        <Link href="https://github.com/cuam3a/resume-app" target="_blank" className='flex justify-center' ><span className="inline-block bg-blue-500 text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 w-3/5 flex flex-row items-center justify-center"><BsGithub className='mr-2'/> Repositorio</span></Link>
+                        <Link href="https://resume-app-two.vercel.app/" target="_blank" className='flex justify-center' ><span className="inline-block bg-blue-500 text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 w-3/5 flex flex-row items-center justify-center"><BsLink className='mr-2'/> Sitio WEB</span></Link>
                     </div>
                     <div className="px-6 pt-4 pb-2">
                         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Next.js</span>
@@ -89,7 +90,7 @@ export default function Portfolio() {
 
                 <div className="rounded overflow-hidden shadow-lg justify-center items-center">
                     <Image
-                        src="https://img2.freepng.es/20180825/qgk/kisspng-enterprise-resource-planning-software-testing-pene-erp-corewin-5b8169ead0f075.7095392615352079148558.jpg"
+                        src="/images/portafolio4.png"
                         alt="Java Script"
                         height={150}
                         width={150}
